afc0d4988a5808dbe9e7e52d4b6fa8a87192211a
Pai Phyo Naing <paiphyonaing93@gmail.com>
2024-08-13 21:21:36 +0700

    error handling

--- script.js
+++ script.js
@@ -24,7 +24,19 @@ function fetchProfile() {
         // Display Info
         profileInfo.innerHTML = `
             <h2>${data.name || "No Name"}</h2>
-
+            <img src="${data.avatar_url}" alt="${data.login}'s avatar" width="100px"">
+            <p><strong>Username:</strong> ${data.login}</p>
+            <p><strong>Bio:</strong> ${data.bio || 'No bio available'}</p>
+            <p><strong>Followers:</strong> ${data.followers}</p>
+            <p><strong>Following:</strong> ${data.following}</p>
+            <p><strong>Public Repos:</strong> ${data.public_repos}</p>
+            <p><strong>Profile URL:</strong> <a href="${data.html_url}"
+            target="_blank">${data.html_url}</a></p>
             `;
     })
+
+    // Error Handling
+    .catch(error => {
+        profileInfo.innerHTML = `<p style="color: red;">An error occurred: ${error.message}</p>`;   
+    });
 }
