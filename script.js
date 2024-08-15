e7a34d5e7c5d20f2c1cd369546e558e7717a5397
Pai Phyo Naing <paiphyonaing93@gmail.com>
2024-08-15 23:19:34 +0700

    updated

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-document.getElementById('fetchProfile').addEventListener('click', fetchDefault);
+document.getElementById('fetchProfile').addEventListener('click', fetchProfile);
 document.getElementById('username').addEventListener('keydown', function(event) {
     if (event.key === 'Enter') {
-        fetchDefault();
+        fetchProfile();
     }
 })
 
@@ -23,48 +23,26 @@ function fetchProfile() {
     .then(data => {
         // Display Info
         profileInfo.innerHTML = `
+        <div class="section-left">
             <h2>${data.name || "No Name"}</h2>
-            <img src="${data.avatar_url}" alt="${data.login}'s avatar" width="100px"">
-            <p>Username: ${data.login}</p>
-            <p>Email: ${data.email || 'No email available'}</p>
-            <p>Bio: ${data.bio || 'No bio available'}</p>
-            <p>Followers: ${data.followers}</p>
-            <p>Following: ${data.following}</p>
-            <p>Public Repos: ${data.public_repos}</p>
-            <p>Profile URL: <a href="${data.html_url}"
-            target="_blank">${data.html_url}</a></p>
+            <img src="${data.avatar_url}" alt="${data.login}'s avatar"">
             <p>Created: ${data.created_at}</p>
+        </div>
+        <div class="section-right">
+            <p><strong>Username</strong>: ${data.login}</p>
+            <p><strong>Email</strong>: ${data.email || 'No email available'}</p>
+            <p><strong>Bio</strong>: ${data.bio || 'No bio available'}</p>
+            <p><strong>Followers</strong>: ${data.followers}</p>
+            <p><strong>Following</strong>: ${data.following}</p>
+            <p><strong>Public Repos</strong>: ${data.public_repos}</p>
+            <p><strong>Profile URL</strong>: <a href="${data.html_url}"
+            target="_blank">${data.html_url}</a></p>
+        </div>
             `;
     })
 
     // Error Handling
     .catch(error => {
-        profileInfo.innerHTML = `<p style="color: red;">An error occurred: ${error.message}</p>`;   
+        profileInfo.innerHTML = `<p class="error">An error occurred: ${error.message}</p>`;   
     });
-}
-
-function fetchDefault() {
-    const username = document.getElementById('username').value;
-    const profileInfo = document.getElementById('profileInfo');
-
-    // Clear Info
-    profileInfo.innerHTML = '';
-
-    profileInfo.innerHTML = `
-    <div class="section-left">
-        <h2>Pai Phyo Naing</h2>
-        <img src="img/search-icon.png" alt="Pai's avatar"   ">
-        <p><strong>Created</strong>: 2024</p>
-    </div>
-    <div class="section-right">
-        <p><strong>Username</strong>: paiphyonaing93</p>
-        <p><strong>Email</strong>: paiphyonaing93@gmail.com</p>
-        <p><strong>Bio</strong>: 'No bio available'</p>
-        <p><strong>Followers</strong>: 1</p>
-        <p><strong>Following</strong>: 0</p>
-        <p><strong>Public Repos</strong>: 11</p>
-        <p><strong>Profile URL</strong>: <a href="https://github.com/paiphyonaing93"
-        target="_blank">https://github.com/paiphyonaing93</a></p>
-    </div>
-    `;
 }
